2be4a5f5f8ab65a7ffa3374eb4773c12832a02a8
SleepyPandas <tommyrobotics1@gmail.com>
2026-03-02 21:40:00 -0500

    feat: implement initial ST7789V3 driver structure with basic initialization and header definitions.

---
+++ ST7789V3_Driver/ST7789V3.c
@@ -0,0 +1,15 @@
+
+/**
+ *
+ * @file         ST7789V3.c
+ * @brief       
+ *               This file will contain the main logic for using the ST7789V3 chips 
+ *
+ */
+
+#include "ST7789V3.h"
+
+void ST7789V3_init() {
+    
+}
+
---
+++ ST7789V3_Driver/ST7789V3.h
@@ -0,0 +1,23 @@
+/**
+ *
+ * @file         ST7789V3.h
+ * @brief        Header for ST7789V3.c file.
+ *               This file contains the common defines of the ST7789V3 driver.
+ *
+ */
+
+#ifndef __ST7789V3_H
+#define __ST7789V3_H
+
+
+#define ST7789V3_WIDTH  ?
+#define ST7789V3_HEIGHT ?
+
+void ST7789V3_init();
+
+
+
+
+#endif /* __ST7789V3_H */
+
+
