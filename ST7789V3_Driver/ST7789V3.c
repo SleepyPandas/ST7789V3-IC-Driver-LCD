840e734ab68ab7a5f3931b13be0e79973a5ab119
SleepyPandas <tommyrobotics1@gmail.com>
2026-03-05 21:04:30 -0500

    feat: add 2 Private Helper Functions to write Data and Commands
    Wake Device Code

--- ST7789V3_Driver/ST7789V3.c
+++ ST7789V3_Driver/ST7789V3.c
@@ -11,10 +11,50 @@
 
 #include "ST7789V3.h"
 
-// void ST7789V3_init() {
-//   /* 1. CHIP SELECT with CS + DC command or Pixel data
-//    * 2. Init Sequence (Wake the display from idle)
-//    * 3. Draw using CA SET RA SET RAMWR 
-//    * 4. Font Rendering Lookup Table (find it)
-//    */
-// }
+
+// ------------ PRIVATE HELPER FUNCTIONS ------------------
+
+
+static void ST7789V3_WriteCommand(ST7789V3_Config *config, uint8_t cmd) {
+  config->set_dc(CMD);
+  config->set_cs(Low);
+  config->spi_write(1, &cmd);
+  config->set_cs(High);
+}
+
+static void ST7789V3_WriteData(ST7789V3_Config *config, uint8_t data) {
+  config->set_dc(DATA);
+  config->set_cs(Low);
+  config->spi_write(1, &data);
+  config->set_cs(High);
+}
+
+
+
+void ST7789V3_init(ST7789V3_Config *config) {
+  //   1. Hardware RST pulse GPIO only, no SPI
+  //   2. Delay(~120ms after RST goes HIGH)
+  //   3. Now start SPI communication
+  //   4. SLPOUT(0x11) + delay
+  //    other stuff.... for initilization Perhaps Based on users config?
+
+  //
+
+  config->set_rst(High);
+  config->delay_ms(120);
+  config->set_rst(Low);
+
+  // SPI communication to WAKE
+  ST7789V3_WriteCommand(config, Sleep_Out);
+  config->delay_ms(120);
+
+  // Color Mode 16 bit TBD
+
+  // Memory Access Control
+
+  // Rotation, color Order default RGB
+  
+  // Display Inversion 
+
+  // Display On and Show Buffer items
+}
